754041dadae32bc359a47b418a73cb205a197b2c
deanthakur <deansthakur@gmail.com>
2024-06-26 00:38:03 -0400

    added function composition operator (.)

--- chapter2/first.hs
+++ chapter2/first.hs
@@ -7,3 +7,16 @@ e = exp 1
 -- Here we define a function
 square :: Double -> Double
 square x = x**2
+
+-- ways to express square of a cosine function
+cosSq::Double -> Double
+cosSq x = square (cos x)
+
+cosSq'::Double -> Double
+cosSq' x = square $ cos x
+
+cosSq''::Double -> Double
+cosSq'' x = (square . cos) x
+
+cosSq'''::Double -> Double
+cosSq''' = square . cos
